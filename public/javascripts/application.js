c47a8aa555f5787e0f8d404fc247ce8ae32d43d8
Alberto Fernández Capel <afcapel@gmail.com>
2010-02-26 12:27:43 +0100

    Modified application.js to avoid strange bug with chrome.
    See: https://rails.lighthouseapp.com/projects/8994/tickets/3803-bug-in-chromium-with-get-parameters-when-doing-a-post-request

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,14 @@
+$.extend({
+	URLEncode:function(c){var o='';var x=0;c=c.toString();var r=/(^[a-zA-Z0-9_.]*)/;
+  while(x<c.length){var m=r.exec(c.substr(x));
+    if(m!=null && m.length>1 && m[1]!=''){o+=m[1];x+=m[1].length;
+    }else{if(c[x]==' ')o+='+';else{var d=c.charCodeAt(x);var h=d.toString(16);
+    o+='%'+(h.length<2?'0':'')+h.toUpperCase();}x++;}}return o;},
+	URLDecode:function(s){var o=s;var binVal,t;var r=/(%[^%]{2})/;
+  while((m=r.exec(o))!=null && m.length>1 && m[1]!=''){b=parseInt(m[1].substr(1),16);
+  t=String.fromCharCode(b);o=o.replace(m[1],t);}return o;}
+});
+
 $(document).ready(function(){
 
 	//Remote links
@@ -8,12 +19,26 @@ $(document).ready(function(){
 	//Style tables
 	$('table tbody tr:odd').addClass('odd');
 	$('table tbody tr:even').addClass('even');
+	
+	var paramsForUrl = function(url){
+		var params = {};
+		var queryString = url.match(/\?(.+)/)[1];
+		var pairs = queryString.split('&');
+		for(var i=0; i < pairs.length ; i++){
+			var param = pairs[i].split('=');
+			params[$.URLDecode(param[0])] = $.URLDecode(param[1]);
+		}
+		return params;
+	};
 
 	var sendRemoteCreate = function(href){
+		var data = paramsForUrl(href);
+		var url = href.replace(/new.*/,'');
 		$.ajax({
-			url: href.replace('/new',''),
+			url: url,
 			dataType: 'script',
-			type: 'POST'
+			type: 'POST',
+			data: data
 		});
 	};
 
